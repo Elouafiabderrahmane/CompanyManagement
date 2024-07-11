096f1e9c61129f19c3057d7e585003450247db6a
Yazid0Hakimi <71094777+Yazid0Hakimi@users.noreply.github.com>
2024-07-11 19:25:33 +0100

    FEAT : updating services

---
+++ server/src/main/java/ma/barakatouna/company_management/service/ProjectServiceImpl.java
@@ -0,0 +1,130 @@
+package ma.barakatouna.company_management.service;
+
+import jakarta.transaction.Transactional;
+import ma.barakatouna.company_management.entities.*;
+import ma.barakatouna.company_management.model.ProjectDTO;
+import ma.barakatouna.company_management.repos.*;
+import ma.barakatouna.company_management.util.NotFoundException;
+import ma.barakatouna.company_management.util.ReferencedWarning;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+
+@Service
+@Transactional
+public class ProjectServiceImpl implements ProjectService{
+
+    private final ProjectRepository projectRepository;
+    private final EmployerRepository employerRepository;
+    private final MaterialRepository materialRepository;
+    private final TaskRepository taskRepository;
+    private final PaymentRepository paymentRepository;
+
+    public ProjectServiceImpl(final ProjectRepository projectRepository,
+                              final EmployerRepository employerRepository,
+                              final MaterialRepository materialRepository, final TaskRepository taskRepository,
+                              final PaymentRepository paymentRepository) {
+        this.projectRepository = projectRepository;
+        this.employerRepository = employerRepository;
+        this.materialRepository = materialRepository;
+        this.taskRepository = taskRepository;
+        this.paymentRepository = paymentRepository;
+    }
+
+    public List<ProjectDTO> findAll() {
+        final List<Project> projects = projectRepository.findAll(Sort.by("id"));
+        return projects.stream()
+                .map(project -> mapToDTO(project, new ProjectDTO()))
+                .toList();
+    }
+
+    public ProjectDTO get(final Long id) {
+        return projectRepository.findById(id)
+                .map(project -> mapToDTO(project, new ProjectDTO()))
+                .orElseThrow(NotFoundException::new);
+    }
+
+    public Long create(final ProjectDTO projectDTO) {
+        final Project project = new Project();
+        mapToEntity(projectDTO, project);
+        return projectRepository.save(project).getId();
+    }
+
+    public void update(final Long id, final ProjectDTO projectDTO) {
+        final Project project = projectRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        mapToEntity(projectDTO, project);
+        projectRepository.save(project);
+    }
+
+    public void delete(final Long id) {
+        projectRepository.deleteById(id);
+    }
+
+    private ProjectDTO mapToDTO(final Project project, final ProjectDTO projectDTO) {
+        projectDTO.setId(project.getId());
+        projectDTO.setName(project.getName());
+        projectDTO.setDescription(project.getDescription());
+        projectDTO.setBudget(project.getBudget());
+        projectDTO.setPaid(project.getPaid());
+        projectDTO.setDone(project.getDone());
+        projectDTO.setStartDate(project.getStartDate());
+        projectDTO.setEndDate(project.getEndDate());
+        projectDTO.setUrl(project.getUrl());
+        projectDTO.setEmployers(project.getEmployers().stream()
+                .map(employer -> employer.getId())
+                .toList());
+        projectDTO.setMaterials(project.getMaterials().stream()
+                .map(material -> material.getId())
+                .toList());
+        return projectDTO;
+    }
+
+    private Project mapToEntity(final ProjectDTO projectDTO, final Project project) {
+        project.setName(projectDTO.getName());
+        project.setDescription(projectDTO.getDescription());
+        project.setBudget(projectDTO.getBudget());
+        project.setPaid(projectDTO.getPaid());
+        project.setDone(projectDTO.getDone());
+        project.setStartDate(projectDTO.getStartDate());
+        project.setEndDate(projectDTO.getEndDate());
+        project.setUrl(projectDTO.getUrl());
+        final List<Employer> employers = employerRepository.findAllById(
+                projectDTO.getEmployers() == null ? Collections.emptyList() : projectDTO.getEmployers());
+        if (employers.size() != (projectDTO.getEmployers() == null ? 0 : projectDTO.getEmployers().size())) {
+            throw new NotFoundException("one of employers not found");
+        }
+        project.setEmployers(new HashSet<>(employers));
+        final List<Material> materials = materialRepository.findAllById(
+                projectDTO.getMaterials() == null ? Collections.emptyList() : projectDTO.getMaterials());
+        if (materials.size() != (projectDTO.getMaterials() == null ? 0 : projectDTO.getMaterials().size())) {
+            throw new NotFoundException("one of materials not found");
+        }
+        project.setMaterials(new HashSet<>(materials));
+        return project;
+    }
+
+    public ReferencedWarning getReferencedWarning(final Long id) {
+        final ReferencedWarning referencedWarning = new ReferencedWarning();
+        final Project project = projectRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        final Task projectTask = taskRepository.findFirstByProject(project);
+        if (projectTask != null) {
+            referencedWarning.setKey("project.task.project.referenced");
+            referencedWarning.addParam(projectTask.getId());
+            return referencedWarning;
+        }
+        final Payment projectPayment = paymentRepository.findFirstByProject(project);
+        if (projectPayment != null) {
+            referencedWarning.setKey("project.payment.project.referenced");
+            referencedWarning.addParam(projectPayment.getId());
+            return referencedWarning;
+        }
+        return null;
+    }
+
+}
---
+++ server/src/main/java/ma/barakatouna/company_management/service/TaskServiceImpl.java
@@ -0,0 +1,108 @@
+package ma.barakatouna.company_management.service;
+
+import jakarta.transaction.Transactional;
+import ma.barakatouna.company_management.entities.Employer;
+import ma.barakatouna.company_management.entities.Project;
+import ma.barakatouna.company_management.entities.Task;
+import ma.barakatouna.company_management.model.TaskDTO;
+import ma.barakatouna.company_management.repos.EmployerRepository;
+import ma.barakatouna.company_management.repos.MaterialRepository;
+import ma.barakatouna.company_management.repos.ProjectRepository;
+import ma.barakatouna.company_management.repos.TaskRepository;
+import ma.barakatouna.company_management.util.NotFoundException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+
+@Service
+@Transactional
+public class TaskServiceImpl implements TaskService{
+
+    private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
+    private final EmployerRepository employerRepository;
+    private final MaterialRepository materialRepository;
+
+    public TaskServiceImpl(final TaskRepository taskRepository,
+                           final ProjectRepository projectRepository, final EmployerRepository employerRepository,
+                           final MaterialRepository materialRepository) {
+        this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
+        this.employerRepository = employerRepository;
+        this.materialRepository = materialRepository;
+    }
+
+    public List<TaskDTO> findAll() {
+        final List<Task> tasks = taskRepository.findAll(Sort.by("id"));
+        return tasks.stream()
+                .map(task -> mapToDTO(task, new TaskDTO()))
+                .toList();
+    }
+
+    public TaskDTO get(final Long id) {
+        return taskRepository.findById(id)
+                .map(task -> mapToDTO(task, new TaskDTO()))
+                .orElseThrow(NotFoundException::new);
+    }
+
+    public Long create(final TaskDTO taskDTO) {
+        final Task task = new Task();
+        mapToEntity(taskDTO, task);
+        return taskRepository.save(task).getId();
+    }
+
+    public void update(final Long id, final TaskDTO taskDTO) {
+        final Task task = taskRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        mapToEntity(taskDTO, task);
+        taskRepository.save(task);
+    }
+
+    public void delete(final Long id) {
+        final Task task = taskRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        // remove many-to-many relations at owning side
+        materialRepository.findAllByTasks(task)
+                .forEach(material -> material.getTasks().remove(task));
+        taskRepository.delete(task);
+    }
+
+    private TaskDTO mapToDTO(final Task task, final TaskDTO taskDTO) {
+        taskDTO.setId(task.getId());
+        taskDTO.setTasktype(task.getTasktype());
+        taskDTO.setDone(task.getDone());
+        taskDTO.setTitle(task.getTitle());
+        taskDTO.setDescription(task.getDescription());
+        taskDTO.setStartingDate(task.getStartingDate());
+        taskDTO.setEndingDate(task.getEndingDate());
+        taskDTO.setProject(task.getProject() == null ? null : task.getProject().getId());
+        taskDTO.setEmployer(task.getEmployer().stream()
+                .map(employer -> employer.getId())
+                .toList());
+        return taskDTO;
+    }
+
+    private Task mapToEntity(final TaskDTO taskDTO, final Task task) {
+        task.setTasktype(taskDTO.getTasktype());
+        task.setDone(taskDTO.getDone());
+        task.setTitle(taskDTO.getTitle());
+        task.setDescription(taskDTO.getDescription());
+        task.setStartingDate(taskDTO.getStartingDate());
+        task.setEndingDate(taskDTO.getEndingDate());
+        final Project project = taskDTO.getProject() == null ? null : projectRepository.findById(taskDTO.getProject())
+                .orElseThrow(() -> new NotFoundException("project not found"));
+        task.setProject(project);
+        final List<Employer> employer = employerRepository.findAllById(
+                taskDTO.getEmployer() == null ? Collections.emptyList() : taskDTO.getEmployer());
+        if (employer.size() != (taskDTO.getEmployer() == null ? 0 : taskDTO.getEmployer().size())) {
+            throw new NotFoundException("one of employer not found");
+        }
+        task.setEmployer(new HashSet<>(employer));
+        return task;
+    }
+
+}
